2dd428a2349151d62fdfb437e928fea9f2c9f929
SetHet <sethet1@gmail.com>
2023-10-05 22:50:49 -0300

    paypal init

--- src/config/index.js
+++ src/config/index.js
@@ -3,5 +3,9 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const config = {
-    port: process.env.PORT
-}
+  port: process.env.PORT || 3000,
+  paypal: {
+    account: process.env.PAYPAL_ACCOUNT,
+    token: process.env.PAYPAL_TOKEN,
+  },
+};
--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 import Express from "express";
 import {config} from "./config/index.js";
+import {} from "./services/paypal.js"
+import {} from "./services/mercadopago.js";
 
 const app = Express()
+app.use(Express.json())
 
 app.get("/", (req, res) => {
     res.send("exitoso el get")
+})
+
+app.get("/pay", (req, res) => {
+    const body = req.body;
+
+
+
+    res.status(200).json({
+        mensaje: "Sistema de pago",
+        body: body,
+    })
 })
 
 app.listen(config.port, (error) => {
