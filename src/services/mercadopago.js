9564f4716a8d32057cd96f86e51994393007910b
SetHet <sethet1@gmail.com>
2023-10-23 01:56:11 -0300

    Update mercadopago.js

--- src/services/mercadopago.js
+++ src/services/mercadopago.js
@@ -1,6 +1,6 @@
 //import MP from "mercadopago";
 import { MercadoPagoConfig, Payment } from "mercadopago";
-import {config} from "../config/index";
+import {config} from "../config/index.js";
 
 const client = new MercadoPagoConfig({ accessToken: config.mercadopago.accessToken });
 
